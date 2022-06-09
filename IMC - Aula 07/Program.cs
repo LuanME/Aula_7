1d3e249af2c50cd82fedb13ebedad49dbdd98a55
Luan Victor Leite da Silva <luans@me.local>
2022-06-09 17:47:23 -0300

    Adicionando Atividade IMC

---
+++ IMC - Aula 07/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string retornarDiagnosticoDoUsuario(double alturaDoUsuario, double pesoDoUsuario)
+{
+    double imc = pesoDoUsuario / (alturaDoUsuario * alturaDoUsuario);
+
+    if (imc < 18.5)
+    {
+        return "Abaixopeso";
+    }else if (imc >= 18.5 && imc <= 24.9)
+    {
+        return "Normal";
+    }
+    else if (imc >= 25 && imc <= 29.9)
+    {
+        return "Sobrepeso";
+    }
+    else if (imc >= 30 && imc < 40 )
+    {
+        return "Obesidade";
+    }
+    else
+    {
+        return "Obesidade grave";
+    }
+
+}
+
+Console.WriteLine($"Seu diagnóstico é {retornarDiagnosticoDoUsuario(1.70, 105)}");
